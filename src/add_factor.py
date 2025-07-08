1e66ff281c0a581414c79b8176096a0a0930b412
jiang926 <2660108983@qq.com>
2025-07-08 13:52:42 +0800

    🆗

--- src/add_factor.py
+++ src/add_factor.py
@@ -5,7 +5,7 @@ import pandas as pd
 # import sys
 #
 # sys.path.append('../')
-from factor_cli.cli import add_cmake_factor, node_factor
+from factor_cli import add_cmake_factor, node_factor
 from database.mysql_database import GetFactorDataAPI
 
 #
